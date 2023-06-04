c3204afc4f90c8ec51f47c60c33d3bf39119064c
Misha <mishagrigoryan@gmail.com>
2023-06-03 21:15:04 -0400

    transfrom works

--- transform.py
+++ transform.py
@@ -22,16 +22,41 @@ conn = psycopg2.connect(
     password=config.pg_password
 )
 cur = conn.cursor()
+conn2 = psycopg2.connect(
+    host=config.pg_host,
+    database="db1",
+    user=config.pg_user,
+    password=config.pg_password
+)
+cur2 = conn2.cursor()
 # After establishing the connection and getting the cursor
 # cur.execute("""
 #     DROP TABLE IF EXISTS reddit_posts;
 # """)
-cur.execute("""
+cur2.execute("""
     CREATE TABLE IF NOT EXISTS reddit_posts_hot_summary (     
         subreddit TEXT,
-        post_upvote_ratio REAL,
+        post_upvote_ratio FLOAT,
         post_comments INT,
         count int
     );
 """)
-conn.commit()
+conn2.commit()
+# Run the query on the first database
+cur.execute("""
+    SELECT subreddit, AVG(post_upvote_ratio) avg_upvote_ratio
+    ,ROUND(AVG(reddit_posts_hot.post_comments)) avg_comment_count
+    ,COUNT(subreddit) 
+    FROM reddit_posts_hot GROUP BY 1 ORDER BY 2 DESC
+""")
+rows = cur.fetchall()
+
+# Insert the results into the second database
+for row in rows:
+    cur2.execute("""
+        INSERT INTO reddit_posts_hot_summary (subreddit, post_upvote_ratio, post_comments, count)
+        VALUES (%s, %s, %s, %s)
+    """, (row[0], row[1], row[2], row[3]))
+
+conn2.commit()
+
